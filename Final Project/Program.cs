bdac45702ca5730c664eb691662c2e50e52ae8a9
Cullin Flynn <cullinflynn@rocketmortgage.com>
2022-01-31 11:28:49 -0500

    added a CW line in Main[]

--- Final Project/Program.cs
+++ Final Project/Program.cs
@@ -14,6 +14,7 @@ namespace Final_Project
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("This line was added by Cullin.");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
