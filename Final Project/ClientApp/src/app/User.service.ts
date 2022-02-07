62d72f2c965bbb701770d57622242f8452b48704
pizzahut111 <flynncu@gmail.com>
2022-02-07 14:17:44 -0500

    added GetUsers method to front end; able to print user names

--- Final Project/ClientApp/src/app/User.service.ts
+++ Final Project/ClientApp/src/app/User.service.ts
@@ -1,16 +1,23 @@
 import { Injectable, Inject } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from './User';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class UserService {
     url: string = "User";
     user?: User;
     users?: User[] = [];
 
-constructor(private http:HttpClient, @Inject('BASE_URL') baseURL: string) {
-    this.url = baseURL + this.url;
- }
+    constructor(private http: HttpClient, @Inject('BASE_URL') baseURL: string) {
+        this.url = baseURL + this.url;
+    }
+    GetUsers(): Observable<any> {
+        return this.http.get(this.url + "/users");
+        console.log(this.url);
+        // let apiurl: string = this.url + "/api";
+        // let result: Observable<any> = this.http.get(apiurl);
+        // return result;
+    }
 
 }
